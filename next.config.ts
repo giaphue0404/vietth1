afd184179504527fb6d3b66b4a297edc1a30f5e5
megait004 <giapfc123@gmail.com>
2026-01-17 16:09:47 +0700

    Remove unoptimized images configuration from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,6 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
     reactCompiler: false,
     poweredByHeader: false,
-    images: {
-        unoptimized: true
-    },
     experimental: {
         serverComponentsHmrCache: false
     }
